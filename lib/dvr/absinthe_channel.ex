8c547fd90dfde33c4e448920cb47b3e0ce69abab
Andrew Thal <467872+athal7@users.noreply.github.com>
2021-08-30 19:03:47 +0000

    Use aliases
    Signed-off-by: GitHub <noreply@github.com>

--- lib/dvr/absinthe_channel.ex
+++ lib/dvr/absinthe_channel.ex
@@ -4,6 +4,8 @@ defmodule DVR.AbsintheChannel do
   """
 
   if Code.ensure_loaded?(Absinthe.Subscription) do
+    alias Absinthe.{Phase, Pipeline, Subscription}
+
     def handle_in("replay", %{"replayId" => replay_id, "subscriptionId" => doc_id}, socket) do
       case DVR.search(replay_id) do
         {:ok, _} ->
@@ -37,7 +39,7 @@ defmodule DVR.AbsintheChannel do
 
     defp doc_id_topics(doc_id, socket) do
       socket.endpoint
-      |> Absinthe.Subscription.registry_name()
+      |> Subscription.registry_name()
       |> Registry.lookup({self(), doc_id})
       |> Enum.map(fn {_self, field_key} -> field_key end)
     end
@@ -46,37 +48,37 @@ defmodule DVR.AbsintheChannel do
       topics
       |> Enum.flat_map(fn topic ->
         socket.endpoint
-        |> Absinthe.Subscription.get(topic)
+        |> Subscription.get(topic)
         |> Enum.filter(fn {id, _doc} -> id == doc_id end)
         |> Enum.map(fn {_id, doc} -> doc end)
       end)
     end
 
-
     # From https://github.com/absinthe-graphql/absinthe/blob/master/lib/absinthe/subscription/local.ex#run_docset
-    alias Absinthe.{Phase, Pipeline}
+
     defp resolve(payload, doc) do
       pipeline =
-          doc.initial_phases
-          |> Pipeline.replace(
-            Phase.Telemetry,
-            {Phase.Telemetry, event: [:subscription, :publish, :start]}
-          )
-          |> Pipeline.without(Phase.Subscription.SubscribeSelf)
-          |> Pipeline.insert_before(
-            Phase.Document.Execution.Resolution,
-            {Phase.Document.OverrideRoot, root_value: payload}
-          )
-          |> Pipeline.upto(Phase.Document.Execution.Resolution)
+        doc.initial_phases
+        |> Pipeline.replace(
+          Phase.Telemetry,
+          {Phase.Telemetry, event: [:subscription, :publish, :start]}
+        )
+        |> Pipeline.without(Phase.Subscription.SubscribeSelf)
+        |> Pipeline.insert_before(
+          Phase.Document.Execution.Resolution,
+          {Phase.Document.OverrideRoot, root_value: payload}
+        )
+        |> Pipeline.upto(Phase.Document.Execution.Resolution)
 
       pipeline = [
         pipeline,
         [
-          Absinthe.Phase.Document.Result,
-          {Absinthe.Phase.Telemetry, event: [:subscription, :publish, :stop]}
+          Phase.Document.Result,
+          {Phase.Telemetry, event: [:subscription, :publish, :stop]}
         ]
       ]
-      Absinthe.Pipeline.run(doc.source, pipeline)
+
+      Pipeline.run(doc.source, pipeline)
     end
   end
 end
