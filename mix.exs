332848cbb37b34e535f5f2ff30e1ae886bf62a50
Andrew Thal <467872+athal7@users.noreply.github.com>
2023-07-25 16:22:58 +0000

    v1.1.1
    Signed-off-by: Andrew Thal <467872+athal7@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DVR.MixProject do
   def project do
     [
       app: :dvr,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       docs: [main: "readme", extras: ["README.md"]],
